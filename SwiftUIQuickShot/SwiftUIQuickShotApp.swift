6df1789d8457a38cd83dbcff3985e5aee318d6e2
Patryk-Jastrzebski <patryk.jastrzebski@iteo.com>
2022-12-28 09:23:05 +0100

    Initial Commit

---
+++ SwiftUIQuickShot/SwiftUIQuickShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIQuickShotApp.swift
+//  SwiftUIQuickShot
+//
+//  Created by Patryk Jastrzębski on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIQuickShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
