ef962f116186a2de587455fe0d318180a5f45724
Piotr Sztefko <p.sztefko@gmail.com>
2022-12-30 16:00:55 +0100

    created initial grid

--- SwiftUIQuickShot/Features/LazyGrids.swift
+++ SwiftUIQuickShot/Features/LazyGrids.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct LazyGrids: View {
+    var vGridLayout = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        gridView
+    }
+}
+
+extension LazyGrids {
+    private var gridView: some View {
+        ScrollView {
+            LazyVGrid(columns: vGridLayout) {
+                ForEach(0..<100) { value in
+                    Rectangle()
+                        .foregroundColor(Color.green)
+                        .frame(height: 50)
+                        .overlay(
+                            Text("\(value)").foregroundColor(.white)
+                        )
+                }
+            }.padding(.all, 10)
+        }
     }
 }
 
