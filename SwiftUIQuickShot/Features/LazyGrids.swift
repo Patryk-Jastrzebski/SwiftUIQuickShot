9074ccf2ef9c48708cd884b9433c592c6ede8972
Piotr Sztefko <p.sztefko@gmail.com>
2022-12-30 11:58:31 +0100

    created lazy grids file

---
+++ SwiftUIQuickShot/Features/LazyGrids.swift
@@ -0,0 +1,20 @@
+//
+//  LazyGrids.swift
+//  SwiftUIQuickShot
+//
+//  Created by Piotr Sztefko on 30/12/2022.
+//
+
+import SwiftUI
+
+struct LazyGrids: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LazyGrids_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyGrids()
+    }
+}
